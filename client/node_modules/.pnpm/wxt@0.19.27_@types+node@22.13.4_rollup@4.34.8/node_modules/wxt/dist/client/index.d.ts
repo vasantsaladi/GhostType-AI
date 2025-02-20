3542233c2174dd6ba68f27b6c6ac4ac15fa97ed0
Vasant Saladi <60614334+vasantsaladi@users.noreply.github.com>
2025-02-20 09:40:59 -0500

    added

---
+++ client/node_modules/.pnpm/wxt@0.19.27_@types+node@22.13.4_rollup@4.34.8/node_modules/wxt/dist/client/index.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Any runtime APIs that use the web extension APIs.
+ *
+ * @module wxt/client
+ */
+export * from './content-scripts';
+export * from './app-config';
+export * from './inject-script';
---
+++ client/node_modules/.pnpm/wxt@0.19.27_@types+node@22.13.4_rollup@4.34.8/node_modules/wxt/dist/core/utils/building/index.d.ts
@@ -0,0 +1,7 @@
+export * from './build-entrypoints';
+export * from './detect-dev-changes';
+export * from './find-entrypoints';
+export * from './group-entrypoints';
+export * from './import-entrypoint';
+export * from './internal-build';
+export * from './rebuild';
